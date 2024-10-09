a1ef42fb444a0f77810f780ff9b5467f3afc2cd3
Armandsikk03 <armand.sikk@tktk.ee>
2024-10-09 21:06:01 +0300

    add week2 code

---
+++ useragevalidation/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Armand!");
+
+//rakendus küsib kasutajalt tema sugu
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust rakendus tervitab kasutajat järgmiselt:
+//"welcome, mr. [kasutaja perekonnanimi] / "Welcome ms. [Kasutaja perekonnanimi]
+
+Console.WriteLine(  "Please select your gender (m/f)");
+char usergender = Char.Parse(Console.ReadLine());//loeb konsoolist andmeid string (sõne) formaadis
+
+Console.WriteLine("Please, enter your last name: ");
+string userLastName = Console.ReadLine();
+
+if (usergender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr.{userLastName}!");
+}
+else if (usergender == 'f')
+{
+    Console.WriteLine($"Welcome Ms.{userLastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome {userLastName} !");
+}
+
